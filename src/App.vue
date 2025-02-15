8099a9057ff6380b3c2250867785419b049fd6c6
Gabzineo <gabdubs12@gmail.com>
2025-02-15 18:27:33 -0300

    estilização do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,47 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  
+  <div class="container bg-primary-subtle pb-4 pt-0 ps-0 pe-0">
+  <header class="p-5 mt-5 mb-5 bg-primary text-white text-center rounded" >
+    <h1>Calculadora no Vue.Js</h1>
+    <p>Escolha a operação que deseja multiplicar</p>
   </header>
+  <form >
+    <div class="row justify-content-center">
+      <div class="col-3">
+        <input type="text" required placeholder="Digite o primeiro numero" class="form-control">
+      </div>
+      <div class="col-3">
+        <input type="text" required placeholder="Digite o segundo numero" class="form-control">
+      </div>
+      <div class="col-1 ">
+        <select class="form-control text-center">
+          <option value="somar">+</option>
+          <option value="subtrair">-</option>
+          <option value="multiplicar">x</option>
+          <option value="dividir">÷</option>
+        </select>
+      </div>
+      </div>
+      <div class="row justify-content-center m-4">
+        <div class="col-3 text-center">
+          <h1>Resposta</h1>
+        </div>
+      </div>
+  </form>
+</div>
 
-  <main>
-    <TheWelcome />
-  </main>
+  
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+body{
+  background-color: red;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
